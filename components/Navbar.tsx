597429b20be3998c2ac98477476541ed1506f127
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2024-12-06 18:56:45 +0200

    ✨ Created Drawer Links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import { FC } from "react";
 import { Logo } from "./Logo";
 import { Menu } from "lucide-react";
-import * as React from "react";
-
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui";
+import { navLinks } from "@/utils/settings";
 
 interface NavbarProps {}
 
@@ -12,18 +11,26 @@ export const Navbar: FC<NavbarProps> = () => {
   return (
     <nav className="border-b py-5 -2">
       <div className="container flex justify-between">
-        <Link href={"/"}>
+        <Link href="/">
           <Logo />
         </Link>
         <Drawer direction="left">
           <DrawerTrigger>
             <Menu />
           </DrawerTrigger>
-          <DrawerContent className="h-screen top-0 left-0 mt-0 w-3/4 rounded-none">
-            <DrawerHeader>
-              <DrawerTitle>Hello</DrawerTitle>
+          <DrawerContent className="h-screen top-0 left-0 mt-0 w-3/4 rounded-none pl-10 pt-48 border-r">
+            <DrawerHeader className="hidden">
+              <DrawerTitle>Nav</DrawerTitle>
+              <DrawerDescription>Nav</DrawerDescription>
             </DrawerHeader>
-            <DrawerDescription>Hello</DrawerDescription>
+            {/* Links */}
+            <div className="flex flex-col gap-8 *:text-3xl">
+              {navLinks.map((link, idx) => (
+                <Link className="w-fit" key={idx} href={link.href}>
+                  {link.name}
+                </Link>
+              ))}
+            </div>
           </DrawerContent>
         </Drawer>
         {/* <Menu /> */}
---
+++ utils/settings.ts
@@ -0,0 +1,8 @@
+export const appName = "Postly";
+
+export const navLinks = [
+  { name: "Discover", href: "/" },
+  { name: "Following", href: "/following" },
+  { name: "Profile", href: "/profile" },
+  { name: "Create", href: "/create" },
+];
