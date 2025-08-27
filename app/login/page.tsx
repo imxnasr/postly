039a510c1009313cdb508a4d1a44e164c02d6d73
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2025-08-27 19:12:23 +0300

    ✨ Remove useActionState

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,49 +1,25 @@
 "use client";
 
 import { login } from "@/actions/login";
+import { Loader } from "@/components/Loader";
 import { Button, Input } from "@/components/ui";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/Form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { startTransition, useActionState, useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
-const initialState: any = { success: false, message: "", errors: {}, inputs: {} };
-
 export default () => {
-  const [state, action, isPending] = useActionState(login, initialState);
-  useEffect(() => {
-    if (state.success) {
-      toast.success(state.message);
-      redirect("/");
-    } else {
-      if (state.message) {
-        toast.error(state.message);
-      }
-    }
-  }, [state]);
-  // defaultValue={state?.inputs?.text}
-  // className={!isPending && state?.errors?.text ? "border-red-500" : ""}
-
-  // const formSchema = z.object({
-  //   usernameOrEmail: z
-  //     .string()
-  //     .min(1, "Username or email is required")
-  //     .refine((val: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) || /^[a-zA-Z0-9_]+$/.test(val), {
-  //       message: "Must be a valid email or username",
-  //     }),
-  //   password: z.string().min(8, "Password must be at least 8 characters"),
-  // });
+  const [isPending, setIsPending] = useState(false);
 
   const formSchema = z.object({
     usernameOrEmail: z
       .string()
       .min(1, "Username or email is required")
-      .refine((val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) || /^[a-zA-Z0-9_]+$/.test(val), {
+      .refine((val: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) || /^[a-zA-Z0-9_]+$/.test(val), {
         message: "Must be a valid email or username",
       }),
     password: z.string().min(8, "Password must be at least 8 characters"),
@@ -60,39 +36,25 @@ export default () => {
     },
   });
 
-  const onSubmit = async (data: FormSchema) => {
-    const formData = new FormData();
-    formData.append("text", data.usernameOrEmail);
-    formData.append("password", data.password);
-    startTransition(() => {
-      action(formData);
-    });
+  const onSubmit = async (formData: FormSchema) => {
+    setIsPending(true);
+    const res = await login(formData);
+    if (res.success) {
+      toast.success(res.message);
+      redirect("/");
+    } else {
+      toast.error(res.message);
+    }
+    setIsPending(false);
   };
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        // onSubmit={(e: any) => {
-        //   e.preventDefault();
-        //   console.log("Submitted");
-        // }}
-        className="max-w-xl m-auto mt-8"
-        autoComplete="on"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-xl m-auto mt-8" autoComplete="on">
         <h3 className="text-2xl font-bold mb-5">Login</h3>
         {/* Inputs */}
         <div className="space-y-3">
           {/* Email Input */}
-          {/* <div className="grid w-full items-center gap-1.5"> */}
-          {/*   <label htmlFor="text">Username or Email</label> */}
-          {/* <Input id="text" name="text" type="text" placeholder="Username or Email" aria-describedby="text-error" /> */}
-          {/* {!isPending && state?.errors?.text && ( */}
-          {/*   <p id="text-error" className="text-red-500 text-sm"> */}
-          {/*     {state.errors.text} */}
-          {/*   </p> */}
-          {/* )} */}
-          {/* </div> */}
           <FormField
             control={form.control}
             name="usernameOrEmail"
@@ -107,21 +69,6 @@ export default () => {
             )}
           />
           {/* Password Input */}
-          {/* <div className="grid w-full items-center gap-1.5"> */}
-          {/* <label htmlFor="password">Password</label> */}
-          {/* <Input */}
-          {/*   id="password" */}
-          {/*   name="password" */}
-          {/*   type="password" */}
-          {/*   placeholder="Password" */}
-          {/*   aria-describedby="password-error" */}
-          {/* /> */}
-          {/* {!isPending && state?.errors?.password && ( */}
-          {/*   <p id="password-error" className="text-red-500 text-sm"> */}
-          {/*     {state.errors.password} */}
-          {/*   </p> */}
-          {/* )} */}
-          {/* </div> */}
           <FormField
             control={form.control}
             name="password"
@@ -142,7 +89,7 @@ export default () => {
             Register
           </Link>
         </p>
-        <Button type="submit" className="w-full mt-6" disabled={false}>
+        <Button type="submit" className="w-full mt-6" disabled={isPending}>
           {isPending ? <Loader /> : "Login"}
         </Button>
       </form>
