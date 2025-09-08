e6bf02b2e497f6ebd37dcfdc61f5e286a930e001
Mahmoud N. <72768187+IMXNasr@users.noreply.github.com>
2025-09-08 22:45:15 +0300

    ✨ Fetch profile data from server

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,36 +1,12 @@
-"use client";
-
-import { Loader } from "@/components/Loader";
 import { Logout } from "@/components/Logout";
 import { Button, Input, Separator, Textarea } from "@/components/ui";
-import { useSession } from "@/lib/auth-client";
-import { useEffect, useState } from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default () => {
-  // const [state, action, isPending] = useActionState(login, initialState);
-  const { data: session, isPending: isSessionPending } = useSession();
+export default async () => {
+  const session = await auth.api.getSession({ headers: await headers() });
+  const userInfo = session?.user;
 
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [bio, setBio] = useState("");
-
-  useEffect(() => {
-    if (session?.user) {
-      const userInfo = session.user;
-      setName(userInfo.name);
-      setUsername(userInfo.username || "");
-      setEmail(userInfo.email);
-      setBio(userInfo.bio || "");
-    }
-  }, [session]);
-
-  if (isSessionPending)
-    return (
-      <div className=" mx-auto">
-        <Loader className="mx-auto" />
-      </div>
-    );
   return (
     <form className="max-w-xl m-auto mt-6">
       <h3 className="text-2xl font-bold mb-5">User info</h3>
@@ -39,22 +15,22 @@ export default () => {
         {/* Name Input */}
         <div className="grid w-full items-center gap-1.5">
           <label htmlFor="name">Name</label>
-          <Input type="text" id="name" placeholder="John Doe" defaultValue={name} />
+          <Input type="text" id="name" placeholder="John Doe" defaultValue={userInfo?.name || ""} />
         </div>
         {/* Username Input */}
         <div className="grid w-full items-center gap-1.5">
           <label htmlFor="username">Username</label>
-          <Input type="text" id="username" placeholder="johndoe" defaultValue={username} />
+          <Input type="text" id="username" placeholder="johndoe" defaultValue={userInfo?.username || ""} />
         </div>
         {/* Email Input */}
         <div className="grid w-full items-center gap-1.5">
           <label htmlFor="email">Email</label>
-          <Input type="email" id="email" placeholder="john.doe@example.com" defaultValue={email} />
+          <Input type="email" id="email" placeholder="john.doe@example.com" defaultValue={userInfo?.email || ""} />
         </div>
         {/* Bio Textarea */}
         <div className="grid w-full items-center gap-1.5">
           <label htmlFor="bio">Bio</label>
-          <Textarea id="bio" placeholder="A short bio..." defaultValue={bio} />
+          <Textarea id="bio" placeholder="A short bio..." defaultValue={userInfo?.bio || ""} />
         </div>
       </div>
       <Button type="submit" className="w-full mt-6" disabled>
